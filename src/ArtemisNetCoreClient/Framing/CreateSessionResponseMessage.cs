040a2e156649b84e48a6a524c10c485b186c3865
Havret <h4vret@gmail.com>
2024-03-09 23:11:14 +0100

    Codec implementation

--- src/ArtemisNetCoreClient/Framing/CreateSessionResponseMessage.cs
+++ src/ArtemisNetCoreClient/Framing/CreateSessionResponseMessage.cs
@@ -2,10 +2,8 @@ namespace ActiveMQ.Artemis.Core.Client.Framing;
 
 internal class CreateSessionResponseMessage : Packet
 {
-    public long ChannelId { get; private set; }
+    public const byte Type = 31;
     public int ServerVersion { get; private set; }
-    
-    public override byte Type { get; } = 31;
     
     public override void Encode(ByteBuffer buffer)
     {
@@ -14,8 +12,6 @@ internal class CreateSessionResponseMessage : Packet
 
     public override void Decode(ByteBuffer buffer)
     {
-        _ = buffer.ReadByte(); // type
-        ChannelId = buffer.ReadLong();
         ServerVersion = buffer.ReadInt();
     }
 }
--- src/ArtemisNetCoreClient/Framing/Packet.cs
+++ src/ArtemisNetCoreClient/Framing/Packet.cs
@@ -2,7 +2,7 @@ namespace ActiveMQ.Artemis.Core.Client.Framing;
 
 internal abstract class Packet
 {
-    public abstract byte Type { get;  }
     public abstract void Encode(ByteBuffer buffer);
+    
     public abstract void Decode(ByteBuffer buffer);
 }
