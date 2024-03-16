7b273b39fefbd948e091ae88f402a16b4f5419d9
Havret <h4vret@gmail.com>
2024-03-16 23:01:57 +0100

    Clean up session

--- src/ArtemisNetCoreClient/Framing/NullResponse.cs
+++ src/ArtemisNetCoreClient/Framing/NullResponse.cs
@@ -2,13 +2,6 @@ namespace ActiveMQ.Artemis.Core.Client.Framing;
 
 internal class NullResponse : Packet
 {
-    public const byte Type = 68;
-    
-    public override void Encode(ByteBuffer buffer)
-    {
-    }
-
-    public override void Decode(ByteBuffer buffer)
-    {
-    }
+    public const byte Type = 21;
+    public override bool IsResponse => true;
 }
