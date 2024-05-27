a827ce23920dc3ca3463bc85ef5758f3fdb9d065
Krzysztof Porebski <h4vret@gmail.com>
2024-05-28 00:05:49 +0200

    Add missing consumer docs (#112)

--- src/ArtemisNetCoreClient/IConsumer.cs
+++ src/ArtemisNetCoreClient/IConsumer.cs
@@ -1,20 +1,29 @@
 namespace ActiveMQ.Artemis.Core.Client;
 
+/// <summary>
+/// A consumer receives messages from ActiveMQ Artemis queues.
+/// </summary>
 public interface IConsumer : IAsyncDisposable
 {
+    /// <summary>
+    /// Receives a message from the broker. This operation will asynchronously wait for a message to be available.
+    /// </summary>
+    /// <param name="cancellationToken">Cancellation token</param>
     ValueTask<ReceivedMessage> ReceiveMessageAsync(CancellationToken cancellationToken = default);
     
     /// <summary>
-    /// Acknowledges all messages received by the consumer so far.
+    /// Acknowledges all messages received by the consumer so far and asynchronously awaits for the confirmation from the broker.
     /// </summary>
-    ValueTask AcknowledgeAsync(MessageDelivery messageDelivery, CancellationToken cancellationToken);
-    
+    /// <param name="messageDelivery">Delivery information of a last message to acknowledge.</param>
+    /// <param name="cancellationToken">Cancellation token.</param>
+    ValueTask AcknowledgeAsync(MessageDelivery messageDelivery, CancellationToken cancellationToken = default);
+
     /// <summary>
-    /// Acknowledges the message.
+    /// Acknowledges the message and asynchronously awaits for the confirmation from the broker.
     /// </summary>
     /// <param name="messageDelivery">Delivery information of a message to acknowledge.</param>
     /// <param name="cancellationToken">Cancellation token.</param>
-    ValueTask IndividualAcknowledgeAsync(in MessageDelivery messageDelivery, CancellationToken cancellationToken);
+    ValueTask IndividualAcknowledgeAsync(in MessageDelivery messageDelivery, CancellationToken cancellationToken = default);
     
     /// <summary>
     /// Acknowledges all messages received by the consumer. It doesn't wait for the confirmation from the broker.
