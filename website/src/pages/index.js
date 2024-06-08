658ebde98db1084dddb66e81b5e72d661a71a728
Havret <h4vret@gmail.com>
2024-06-08 19:45:00 +0200

    Add getting started docs

--- website/src/pages/index.js
+++ website/src/pages/index.js
@@ -57,7 +57,7 @@ function Home() {
   return (
     <Layout
       title={siteConfig.title}
-      description="Unofficial ActiveMQ Artemis .NET Client for .NET Core and .NET Framework">
+      description="Unofficial ActiveMQ Artemis .NET CORE Client">
       <header className={classnames('hero hero--primary', styles.heroBanner)}>
         <div className="container">
           <h1 className="hero__title">{siteConfig.title}</h1>
