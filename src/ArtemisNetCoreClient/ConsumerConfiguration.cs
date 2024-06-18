292805a16970d8fbfa12038fba1c2db44966ac3e
Krzysztof Porebski <h4vret@gmail.com>
2024-06-18 23:44:13 +0200

    Add docs for producer and consumer configuration types (#134)

--- src/ArtemisNetCoreClient/ConsumerConfiguration.cs
+++ src/ArtemisNetCoreClient/ConsumerConfiguration.cs
@@ -1,6 +1,12 @@
 namespace ActiveMQ.Artemis.Core.Client;
 
+/// <summary>
+/// Provides configuration options for creating sessions in an ActiveMQ Artemis message consumer.
+/// </summary>
 public class ConsumerConfiguration
 {
+    /// <summary>
+    /// Gets or sets the name of the queue to consume messages from.
+    /// </summary>
     public required string QueueName { get; init; }
 }
--- src/ArtemisNetCoreClient/ProducerConfiguration.cs
+++ src/ArtemisNetCoreClient/ProducerConfiguration.cs
@@ -1,7 +1,17 @@
 namespace ActiveMQ.Artemis.Core.Client;
 
+/// <summary>
+/// Provides configuration options for creating sessions in an ActiveMQ Artemis message producer.
+/// </summary>
 public class ProducerConfiguration
 {
+    /// <summary>
+    /// Gets or sets the name of the address to send messages to.
+    /// </summary>
     public required string Address { get; init; }
+    
+    /// <summary>
+    /// Gets or sets the routing type to use when sending messages.
+    /// </summary>
     public RoutingType? RoutingType { get; init; }
 }
